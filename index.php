90bc73410c0d14a18089910ed0d1dab3096b3ab6
Rere <renitadewi55@gmail.com>
2019-05-25 10:58:56 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aperiam eaque nemo voluptatum voluptas libero reiciendis cum, quis perspiciatis natus molestiae ipsa aspernatur omnis id adipisci incidunt ipsum dolor aut?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
